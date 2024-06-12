7ba3d62865751ff06a19a4ac7f2da0868f5a7ce8
gdupont <gdupont.pro@gmail.com>
2024-06-12 14:51:05 +0200

    setup base of our vault contract

---
+++ src/Vault.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity "0.8.25";
+
+import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {ERC20PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
+contract Vault is
+    ERC4626Upgradeable,
+    ERC20BurnableUpgradeable,
+    ERC20PausableUpgradeable,
+    ERC20PermitUpgradeable,
+    AccessControlUpgradeable
+{
+    function decimals()
+        public
+        view
+        override(ERC4626Upgradeable, ERC20Upgradeable)
+        returns (uint8)
+    {
+        return ERC4626Upgradeable.decimals();
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override(ERC20PausableUpgradeable, ERC20Upgradeable) {
+        return ERC20Upgradeable._update(from, to, value);
+    }
+}
