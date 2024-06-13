0bb89ff3df5a03caf7d10b3bfaf0efc276442091
gdupont <gdupont.pro@gmail.com>
2024-06-13 14:47:57 +0200

    feat: erc7540 interface

---
+++ src/interfaces/IERC7540Redeem.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity "0.8.25";
+
+interface IERC7540Redeem {
+    event RedeemRequest(
+        address indexed controller,
+        address indexed owner,
+        uint256 indexed requestId,
+        address sender,
+        uint256 shares
+    );
+
+    function requestRedeem(
+        uint256 shares,
+        address operator,
+        address owner
+    ) external returns (uint256 requestId);
+
+    function pendingRedeemRequest(
+        address owner
+    ) external view returns (uint256 shares);
+
+    function claimableRedeemRequest(
+        address owner
+    ) external view returns (uint256 shares);
+}
