fbe1b79c488da524068874e6e2a2c9b525ee937d
gdupont <gdupont.pro@gmail.com>
2024-06-18 17:11:38 +0200

    feat: put all vault variable in the erc7201 storage

--- src/ERC7540Redeem.sol
+++
@@ -1,6 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity "0.8.25";
-import {IERC7540Redeem} from "./interfaces/IERC7540Redeem.sol";
-import {ERC7540Upgradeable} from "./ERC7540.sol";
-
-abstract contract ERC7540Redeem is IERC7540Redeem, ERC7540Upgradeable {}
