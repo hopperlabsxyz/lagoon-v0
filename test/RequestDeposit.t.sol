42b49df67242d882dfc6e66661a0ae2a8ed8b069
gdupont <gdupont.pro@gmail.com>
2024-06-24 15:14:09 +0200

    change Base.t.sol file name to Base.sol

--- test/RequestDeposit.t.sol
+++ test/RequestDeposit.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {Vault} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {BaseTest} from "./Base.t.sol";
+import {BaseTest} from "./Base.sol";
 
 contract TestRequestDeposit is BaseTest {
     function setUp() public {
