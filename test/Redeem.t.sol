1c082c340fb895c5a8c40a6b42eb57c554948edc
gdupont <gdupont.pro@gmail.com>
2024-07-04 20:07:08 +0200

    test redeem and mint

---
+++ test/Redeem.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "forge-std/Test.sol";
+import {Vault} from "@src/Vault.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {BaseTest} from "./Base.sol";
+
+contract TestRedeem is BaseTest {
+    function setUp() public {
+        dealAndApproveAndWhitelist(user1.addr);
+    }
+
+    function test_redeem() public {
+        uint256 userBalance = assetBalance(user1.addr);
+        requestDeposit(userBalance, user1.addr);
+        updateAndSettle(0);
+        assertEq(vault.maxDeposit(user1.addr), userBalance);
+        uint256 shares = deposit(userBalance, user1.addr);
+        assertEq(shares, vault.balanceOf(user1.addr));
+        assertEq(shares, userBalance);
+        requestRedeem(shares, user1.addr);
+        assertEq(vault.claimableRedeemRequest(vault.epochId(), user1.addr), 0);
+
+        updateAndSettle(userBalance + 100);
+        assertEq(vault.maxRedeem(user1.addr), shares);
+        uint256 assets = redeem(shares, user1.addr);
+        assertEq(assets, assetBalance(user1.addr));
+        assertEq(vault.maxRedeem(user1.addr), 0);
+        assertEq(vault.epochId(), 3);
+        assertEq(vault.claimableRedeemRequest(vault.epochId(), user1.addr), 0);
+    }
+}
