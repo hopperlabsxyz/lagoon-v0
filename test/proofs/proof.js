be8836797db2d89157570a94ce9382b3831ebd48
Julien FRANCAIS <jfrancai@student.42.fr>
2024-08-14 11:21:02 +0200

    wip multi config setup

--- test/proofs/proof.js
+++ test/proofs/proof.js
@@ -2,38 +2,39 @@ const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 const fs = require("fs");
 
 // This script allows you to generate all proofs for a given
-// tree and set of accounts
+// set of trees and set of accounts
 //
 // Please run tree.js before runing this script
-// You can change the accounts you want to based you tree on
-// in config.json file
+// Update config.json to change the whitelisted accounts for
+// each config
 
-const treeOutput = "output/tree.json";
-const proofsOutput = "output/proofs.json";
+function createProofs(pathId, accounts) {
+  const treePath = `output/tree_${pathId}.json`;
+  const proofsPath = `output/proofs_${pathId}.json`;
+  const tree = StandardMerkleTree.load(
+    JSON.parse(fs.readFileSync(treePath, "utf8"))
+  );
 
-const { accounts } = JSON.parse(fs.readFileSync("./config.json", "utf8"));
-
-const tree = StandardMerkleTree.load(
-  JSON.parse(fs.readFileSync(treeOutput, "utf8"))
-);
-
-function getProof(_account) {
-  for (const [key, [account]] of tree.entries()) {
-    if (account === _account) {
-      const proof = tree.getProof(key);
-      return { account, proof };
+  const getProof = (_account) => {
+    for (const [key, [account]] of tree.entries()) {
+      if (account === _account) {
+        const proof = tree.getProof(key);
+        return { account, proof };
+      }
     }
-  }
+  };
+  const proofs = accounts.map(([a]) => getProof(a));
+  const root = tree.root;
+
+  console.log(`Config: ${pathId}`);
+  console.log(`Root: ${root}`);
+  console.log("-----------------");
+
+  fs.writeFileSync(proofsPath, JSON.stringify({ root, proofs }, null, 2));
 }
 
-const proofs = [];
-for (const [account] of accounts) {
-  proofs.push(getProof(account));
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+
+for (const [key, accounts] of Object.entries(config)) {
+  createProofs(key, accounts);
 }
-
-fs.writeFileSync(
-  proofsOutput,
-  JSON.stringify({ root: tree.root, proofs }, null, 2)
-);
-
-console.log(`Success! Proofs written to ${proofsOutput}`);
