ed0840f04e0c311eb961522abd5975b088057fd7
Julien FRANCAIS <jfrancai@student.42.fr>
2024-08-29 15:17:41 +0200

    migrate from solidity 0.8.25 => 0.8.26

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity "0.8.25";
+pragma solidity "0.8.26";
 
 // import "forge-std/Test.sol";
 import {ERC7540Upgradeable, EpochData} from "./ERC7540.sol";
