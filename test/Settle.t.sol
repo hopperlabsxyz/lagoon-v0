e2932be3c77e83d9279dbccc804b27364a02938d
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-04 18:25:57 +0200

    bug fix: it was possible to settle without a first nav and uint overflow was happening if settling multiple times with same nav

--- test/Settle.t.sol
+++ test/Settle.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.25;
 
 import "forge-std/Test.sol";
-import {Vault} from "@src/Vault.sol";
+import {Vault, NavIsMissing} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {BaseTest} from "./Base.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
@@ -11,24 +11,46 @@ using Math for uint256;
 
 contract TestSettle is BaseTest {
     function setUp() public {
+        // State after setup:
+        // user1:
+        //  - 50_000 shares
+        //  - 50_000 assets
+        // user2:
+        //  - 100_000 assets
         setUpVault(0, 0, 0);
+
+        // deals 100_000 assets to user1
         dealAndApproveAndWhitelist(user1.addr);
+
+        // 100_000
         uint256 user1Assets = assetBalance(user1.addr);
+
+        // 50_000 assets deposit request
         requestDeposit(user1Assets / 2, user1.addr);
+
+        // deals 100_000 assets to user2
         dealAndApproveAndWhitelist(user2.addr);
 
+        // Settlement:
+        // user1:
+        //  - 50_000 claimable shares
+        //  - 50_000 assets
+        // user2:
+        //  - 100_000 assets
         updateAndSettle(0);
+
+        // user1 claims 50_000 shares
         deposit(user1Assets / 2, user1.addr);
     }
 
-    function test_settle() public {
+    function test_simple_settle() public {
         uint256 user1Assets = assetBalance(user1.addr);
-
         uint256 user1Shares = vault.balanceOf(user1.addr);
         uint256 user2Assets = IERC20(vault.asset()).balanceOf(user2.addr);
 
         requestRedeem(user1Shares, user1.addr);
         requestDeposit(user2Assets, user2.addr);
+
         uint256 totalAssets = vault.totalAssets();
         uint256 totalSupply = vault.totalSupply();
 
@@ -40,8 +62,11 @@ contract TestSettle is BaseTest {
 
         // totalAssets when settle-redeem:
         uint256 totalAssetsWhenRedeem = totalAssetsWhenDeposit + user2Assets;
-        uint256 user2Shares =
-            user2Assets.mulDiv(totalSupplyWhenDeposit + 1, totalAssetsWhenDeposit + 1, Math.Rounding.Floor);
+        uint256 user2Shares = user2Assets.mulDiv(
+            totalSupplyWhenDeposit + 1,
+            totalAssetsWhenDeposit + 1,
+            Math.Rounding.Floor
+        );
         uint256 totalSupplyWhenRedeem = totalSupplyWhenDeposit + user2Shares;
         redeem(user1Shares, user1.addr);
         deposit(user2Assets, user2.addr);
@@ -49,7 +74,12 @@ contract TestSettle is BaseTest {
         // user1 assets: user1Assets + user1Shares.muldiv(75*1e6 + 1, 50e1e6 + 1, Math.Round.floor)
         assertEq(
             user1NewAssets,
-            user1Assets + user1Shares.mulDiv(totalAssetsWhenRedeem, totalSupplyWhenRedeem, Math.Rounding.Floor)
+            user1Assets +
+                user1Shares.mulDiv(
+                    totalAssetsWhenRedeem,
+                    totalSupplyWhenRedeem,
+                    Math.Rounding.Floor
+                )
         );
     }
 
@@ -79,6 +109,114 @@ contract TestSettle is BaseTest {
         vm.expectRevert();
         vault.settleRedeem();
         vm.stopPrank();
+    }
+
+    function test_settle_deposit_without_totalAssets_update_reverts() public {
+        setUpVault(0, 0, 0);
+
+        dealAndApproveAndWhitelist(user1.addr);
+
+        uint256 user1Assets = assetBalance(user1.addr);
+
+        requestDeposit(user1Assets / 2, user1.addr);
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleDeposit();
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+        vm.prank(vault.safe());
+        vault.settleDeposit();
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleDeposit();
+
+        uint256 expectedDepositId = vault.depositId();
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+        assertEq(vault.depositId(), expectedDepositId, "wrong depositId 1");
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+        assertEq(vault.depositId(), expectedDepositId, "wrong depositId 2");
+
+        uint256 userRequestId = requestDeposit(user1Assets / 2, user1.addr);
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+
+        assertEq(userRequestId, expectedDepositId, "wrong userRequestId");
+        assertEq(vault.depositId(), expectedDepositId + 2, "wrong depositId 3");
+
+        vm.prank(vault.safe());
+        vault.settleDeposit();
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleDeposit();
+    }
+
+    function test_settle_redeem_totalAssets_update_reverts() public {
+        setUpVault(0, 0, 0);
+
+        dealAndApproveAndWhitelist(user1.addr);
+
+        uint256 user1Assets = assetBalance(user1.addr);
+
+        requestDeposit(user1Assets / 2, user1.addr);
+
+        updateAndSettle(0);
+
+        vm.prank(user1.addr);
+        uint256 user1Shares = vault.deposit(user1Assets / 2, user1.addr);
+
+        requestRedeem(user1Shares / 2, user1.addr);
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleRedeem();
+
+        updateTotalAssets(user1Assets / 2);
+
+        vm.warp(block.timestamp + 1 days);
+        vm.prank(vault.safe());
+        vault.settleRedeem();
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleRedeem();
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleDeposit();
+
+        uint256 expectedRedeemId = vault.redeemId();
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+        assertEq(vault.redeemId(), expectedRedeemId, "wrong redeemId 1");
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+        assertEq(vault.redeemId(), expectedRedeemId, "wrong redeemId 2");
+
+        uint256 userRequestId = requestRedeem(user1Shares / 2, user1.addr);
+
+        updateTotalAssets(0);
+        vm.warp(block.timestamp + 1 days);
+
+        assertEq(userRequestId, expectedRedeemId, "wrong userRequestId");
+        assertEq(vault.redeemId(), expectedRedeemId + 2, "wrong redeemId 3");
+
+        vm.prank(vault.safe());
+        vault.settleRedeem();
+
+        vm.prank(vault.safe());
+        vm.expectRevert(NavIsMissing.selector);
+        vault.settleRedeem();
     }
 
     // function test_settleAfterUpdate_TooLate() public {
