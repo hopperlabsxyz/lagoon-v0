b8ba24c87edb489ad954e10b27edf1f69e7cc72f
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-11 10:41:07 +0200

    test: RequestIdNotClaimable

--- test/Withdraw.t.sol
+++ test/Withdraw.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {Vault} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ERC7540InvalidOperator} from "@src/ERC7540.sol";
+import {ERC7540InvalidOperator, RequestIdNotClaimable} from "@src/ERC7540.sol";
 
 import {BaseTest} from "./Base.sol";
 
@@ -55,6 +55,27 @@ contract TestWithdraw is BaseTest {
             0,
             "claimableRedeemRequest should be 0"
         );
+    }
+
+    function test_withdraw_revertIfRequestIdNotClaimable() public {
+        uint256 userBalance = assetBalance(user1.addr);
+        requestDeposit(userBalance, user1.addr);
+        updateAndSettle(0);
+        assertEq(vault.maxDeposit(user1.addr), userBalance);
+        uint256 sharesObtained = deposit(userBalance, user1.addr);
+        assertEq(sharesObtained, vault.balanceOf(user1.addr));
+        assertEq(sharesObtained, userBalance);
+        requestRedeem(sharesObtained, user1.addr);
+
+        assertEq(vault.claimableRedeemRequest(vault.redeemId(), user1.addr), 0);
+        uint256 assetsToWithdraw = vault.convertToAssets(
+            sharesObtained,
+            vault.redeemId()
+        );
+
+        vm.prank(user1.addr);
+        vm.expectRevert(RequestIdNotClaimable.selector);
+        vault.withdraw(assetsToWithdraw, user1.addr, user1.addr);
     }
 
     function test_withdraw_revertIfNotOperator() public {
