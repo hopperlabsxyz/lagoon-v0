e995bb855e531faccf33780acbcf4ddc33fb5e76
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-11 08:47:22 +0200

    test: onlyOperator modifier

--- test/Mint.t.sol
+++ test/Mint.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {Vault} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC7540InvalidOperator} from "@src/ERC7540.sol";
 
 import {BaseTest} from "./Base.sol";
 
@@ -21,7 +22,10 @@ contract TestMint is BaseTest {
         uint256 claimableAssets = vault.claimableDepositRequest(0, user1.addr);
         uint256 assetsClaimed = mint(12, user1.addr);
         assertEq(12, vault.balanceOf(user1.addr));
-        uint256 claimableAssetsAfter = vault.claimableDepositRequest(0, user1.addr);
+        uint256 claimableAssetsAfter = vault.claimableDepositRequest(
+            0,
+            user1.addr
+        );
         assertEq(claimableAssetsAfter + assetsClaimed, claimableAssets);
         assertLt(claimableAssetsAfter, claimableAssets);
     }
@@ -37,8 +41,17 @@ contract TestMint is BaseTest {
         uint256 claimableAssets = vault.claimableDepositRequest(0, user1.addr);
         uint256 assetsClaimed = mint(12, user1.addr, user2.addr, user1.addr);
         assertEq(12, vault.balanceOf(user1.addr));
-        uint256 claimableAssetsAfter = vault.claimableDepositRequest(0, user1.addr);
+        uint256 claimableAssetsAfter = vault.claimableDepositRequest(
+            0,
+            user1.addr
+        );
         assertEq(claimableAssetsAfter + assetsClaimed, claimableAssets);
         assertLt(claimableAssetsAfter, claimableAssets);
+    }
+
+    function test_mint_revertIfNotOperator() public {
+        vm.prank(user2.addr);
+        vm.expectRevert(ERC7540InvalidOperator.selector);
+        vault.mint(42, user1.addr, user1.addr);
     }
 }
