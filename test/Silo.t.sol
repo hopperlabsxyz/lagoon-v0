4bc1688238f1298c796bfe6ff3754a5d46645bc4
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-11 11:58:42 +0200

    test: silo constructor + vault infinite approval on silo

---
+++ test/Silo.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "forge-std/Test.sol";
+import {Vault} from "@src/Vault.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Silo} from "@src/Silo.sol";
+
+import {BaseTest} from "./Base.sol";
+
+contract TestSilo is BaseTest {
+    function setUp() public {
+        setUpVault(0, 0, 0);
+    }
+
+    function test_constructorGivesInfiniteApprovalToMsgSender() public {
+        vm.prank(user1.addr);
+        Silo silo = new Silo(underlying);
+        uint256 allowance = underlying.allowance(address(silo), user1.addr);
+        assertEq(allowance, type(uint256).max);
+    }
+
+    function test_vaultHasInfiniteApprovalOnPendingSilo() public view {
+        uint256 allowance = underlying.allowance(
+            vault.pendingSilo(),
+            address(vault)
+        );
+        assertEq(allowance, type(uint256).max);
+    }
+}
