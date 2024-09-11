1f8261e9f5e6f2cc3b27ac3c6ee77eabbf81d383
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-11 10:41:07 +0200

    test: RequestIdNotClaimable

--- test/Mint.t.sol
+++ test/Mint.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {Vault} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ERC7540InvalidOperator} from "@src/ERC7540.sol";
+import {ERC7540InvalidOperator, RequestIdNotClaimable} from "@src/ERC7540.sol";
 
 import {BaseTest} from "./Base.sol";
 
@@ -53,5 +53,13 @@ contract TestMint is BaseTest {
         vm.prank(user2.addr);
         vm.expectRevert(ERC7540InvalidOperator.selector);
         vault.mint(42, user1.addr, user1.addr);
+    }
+
+    function test_mint_revertIfRequestIdNotClaimable() public {
+        uint256 userBalance = assetBalance(user1.addr);
+        requestDeposit(userBalance, user1.addr);
+        vm.prank(user1.addr);
+        vm.expectRevert(RequestIdNotClaimable.selector);
+        vault.mint(userBalance, user1.addr, user1.addr);
     }
 }
