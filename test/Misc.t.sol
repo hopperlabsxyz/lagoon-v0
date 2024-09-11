e1dc121b174be82baadf88afc42c18ecd7b8ee14
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-11 09:07:15 +0200

    adding error checks for preview functions

--- test/Misc.t.sol
+++ test/Misc.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.25;
 import "forge-std/Test.sol";
 import {Vault} from "@src/Vault.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC7540PreviewRedeemDisabled, ERC7540PreviewDepositDisabled, ERC7540PreviewMintDisabled, ERC7540PreviewWithdrawDisabled} from "@src/ERC7540.sol";
 
 import {BaseTest} from "./Base.sol";
 
@@ -14,22 +15,22 @@ contract TestMisc is BaseTest {
     }
 
     function test_previewDeposit() public {
-        vm.expectRevert();
+        vm.expectRevert(ERC7540PreviewDepositDisabled.selector);
         vault.previewDeposit(0);
     }
 
     function test_previewWithdraw() public {
-        vm.expectRevert();
+        vm.expectRevert(ERC7540PreviewWithdrawDisabled.selector);
         vault.previewWithdraw(0);
     }
 
     function test_previewMint() public {
-        vm.expectRevert();
+        vm.expectRevert(ERC7540PreviewMintDisabled.selector);
         vault.previewMint(0);
     }
 
     function test_previewRedeem() public {
-        vm.expectRevert();
+        vm.expectRevert(ERC7540PreviewRedeemDisabled.selector);
         vault.previewRedeem(0);
     }
 
