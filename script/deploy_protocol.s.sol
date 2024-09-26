b3afadfc612bc5dd8ff08005249d919ae3223184
Julien FRANCAIS <jfrancai@student.42.fr>
2024-09-26 16:02:45 +0200

    update: deploy_protocol script

--- script/deploy_protocol.s.sol
+++ script/deploy_protocol.s.sol
@@ -9,18 +9,29 @@ import {Script, console} from "forge-std/Script.sol";
 
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-contract Deploy is Script {
+/*
+
+> How to deploy this script:
+
+source .env && \
+forge clean && \
+forge script script/deploy_protocol.s.sol \
+  --chain-id $CHAIN_ID \
+  --rpc-url $RPC_URL \
+  --tc DeployProtocol \
+  --account defaultKey \ # put your account key here
+  --etherscan-api-key $ETHERSCAN_API_KEY \
+  --verify
+
+*/
+
+contract DeployProtocol is Script {
     address DAO = vm.envAddress("DAO");
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address PROXY_ADMIN = vm.envAddress("PROXY_ADMIN");
 
-        vm.startBroadcast(deployerPrivateKey);
-
-        FeeRegistry feeRegistry = new FeeRegistry();
-        feeRegistry.initialize(DAO, DAO);
-
+        vm.startBroadcast();
         TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(
             payable(
                 Upgrades.deployTransparentProxy(
@@ -34,18 +45,7 @@ contract Deploy is Script {
                 )
             )
         );
-
         console.log("FeeRegistry proxy address: ", address(proxy));
-
         vm.stopBroadcast();
-
-        // Mainnet
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id
-        // 1 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify
-        // #--broadcast
-        // Sepolia
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id
-        // 534351 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL_SEPOLIA} --etherscan-api-key ${ETHERSCAN_API_KEY}
-        // --verify #--broadcast
     }
 }
