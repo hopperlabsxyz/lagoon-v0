2cfbc2f5d360f8ba779627c246692a27a1e44053
Julien FRANCAIS <jfrancai@student.42.fr>
2025-02-20 09:08:03 +0100

    wip

---
+++ src/Vault.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity "0.8.26";
+
+import "@src/vault0.1.0/ERC7540.sol";
+
+////////////// DO NOT REMOVE THIS FILE //////////////////
+
+contract Vault {
+    constructor() {}
+    function doNotRemove() public {}
+}
---
+++ test/Upgradability.t.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import "forge-std/Test.sol";
+
+import {Vault} from "@src/vault0.1.0/Vault.sol";
+import {Vault as Vault0_2_1} from "@src/vault0.2.1/Vault.sol";
+
+import {Options, Upgrades} from "@openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {FeeRegistry} from "@src/protocol/FeeRegistry.sol";
+
+import {Test} from "forge-std/Test.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+
+contract Upgradable is Test {
+    // ERC20 tokens
+    string network = vm.envString("NETWORK");
+    ERC20Permit immutable USDC = ERC20Permit(vm.envAddress(string.concat("USDC_", network)));
+    ERC20 immutable WETH = ERC20(vm.envAddress(string.concat("WETH_", network)));
+    address immutable WRAPPED_NATIVE_TOKEN = vm.envAddress(string.concat("WRAPPED_NATIVE_TOKEN_", network));
+    ERC20 immutable WBTC = ERC20(vm.envAddress(string.concat("WBTC_", network)));
+    ERC20 immutable ETH = ERC20(vm.envAddress(string.concat("ETH_", network)));
+
+    uint8 decimalsOffset = 0;
+
+    string underlyingName = vm.envString("UNDERLYING_NAME");
+
+    FeeRegistry feeRegistry;
+    string vaultName = "vault_";
+    string vaultSymbol = "hop_vault_";
+    uint256 rateUpdateCooldown = 1 days;
+
+    //Underlying
+    ERC20 underlying = ERC20(vm.envAddress(string.concat(underlyingName, "_", network)));
+    ERC20Permit immutable underlyingPermit;
+
+    // Users
+    VmSafe.Wallet user1 = vm.createWallet("user1");
+    VmSafe.Wallet user2 = vm.createWallet("user2");
+    VmSafe.Wallet user3 = vm.createWallet("user3");
+    VmSafe.Wallet user4 = vm.createWallet("user4");
+    VmSafe.Wallet user5 = vm.createWallet("user5");
+    VmSafe.Wallet user6 = vm.createWallet("user6");
+    VmSafe.Wallet user7 = vm.createWallet("user7");
+    VmSafe.Wallet user8 = vm.createWallet("user8");
+    VmSafe.Wallet user9 = vm.createWallet("user9");
+    VmSafe.Wallet user10 = vm.createWallet("user10");
+    VmSafe.Wallet owner = vm.createWallet("owner");
+    VmSafe.Wallet safe = vm.createWallet("safe");
+    VmSafe.Wallet valuationManager = vm.createWallet("valuationManager");
+    VmSafe.Wallet admin = vm.createWallet("admin");
+    VmSafe.Wallet feeReceiver = vm.createWallet("feeReceiver");
+    VmSafe.Wallet dao = vm.createWallet("dao");
+    VmSafe.Wallet whitelistManager = vm.createWallet("whitelistManager");
+
+    VmSafe.Wallet[] users;
+
+    address[] whitelistInit = new address[](0);
+    bool enableWhitelist = true;
+
+    // Wallet
+    VmSafe.Wallet address0 = VmSafe.Wallet({addr: address(0), publicKeyX: 0, publicKeyY: 0, privateKey: 0});
+
+    int256 immutable bipsDividerSigned = 10_000;
+
+    constructor() {
+        vaultName = string.concat(vaultName, underlyingName);
+        vaultSymbol = string.concat(vaultSymbol, underlyingName);
+
+        vm.label(address(USDC), "USDC");
+        vm.label(address(WETH), "WETH");
+        vm.label(address(ETH), "ETH");
+        vm.label(address(WBTC), "WBTC");
+
+        users.push(user1);
+        users.push(user2);
+        users.push(user3);
+        users.push(user4);
+        users.push(user5);
+        users.push(user6);
+        users.push(user7);
+        users.push(user8);
+        users.push(user9);
+        users.push(user10);
+    }
+
+    function _beaconDeploy(
+        string memory contractName,
+        address _owner,
+        Options memory opts
+    ) internal returns (UpgradeableBeacon) {
+        return UpgradeableBeacon(Upgrades.deployBeacon(contractName, _owner, opts));
+    }
+
+    function _proxyDeploy(UpgradeableBeacon beacon, Vault.InitStruct memory v) internal returns (address) {
+        BeaconProxy proxy =
+            BeaconProxy(payable(Upgrades.deployBeaconProxy(address(beacon), abi.encodeCall(Vault.initialize, v))));
+
+        return address(proxy);
+    }
+
+    function test_upgradeable() public {
+        feeRegistry = new FeeRegistry(false);
+        feeRegistry.initialize(dao.addr, dao.addr);
+
+        vm.prank(dao.addr);
+        feeRegistry.updateDefaultRate(50);
+
+        UpgradeableBeacon beacon;
+        Vault.InitStruct memory v = Vault.InitStruct({
+            underlying: underlying,
+            name: vaultName,
+            symbol: vaultSymbol,
+            safe: safe.addr,
+            whitelistManager: whitelistManager.addr,
+            valuationManager: valuationManager.addr,
+            admin: admin.addr,
+            feeReceiver: feeReceiver.addr,
+            feeRegistry: address(feeRegistry),
+            managementRate: 200,
+            performanceRate: 2000,
+            wrappedNativeToken: WRAPPED_NATIVE_TOKEN,
+            rateUpdateCooldown: rateUpdateCooldown,
+            enableWhitelist: enableWhitelist
+        });
+
+        Options memory opts;
+        opts.constructorData = abi.encode(true);
+        beacon = _beaconDeploy("vault0.1.0/Vault.sol:Vault", owner.addr, opts);
+        address vault = _proxyDeploy(beacon, v);
+        console.log("vault address:", vault);
+        opts.constructorData = abi.encode(false);
+        vm.startPrank(owner.addr);
+        Upgrades.upgradeBeacon(address(beacon), "vault0.2.0/Vault.sol:Vault", opts);
+        Upgrades.upgradeBeacon(address(beacon), "vault0.2.1/Vault.sol:Vault", opts);
+        assertEq(keccak256(abi.encode(Vault0_2_1(vault).version())), keccak256(abi.encode("v0.2.1")));
+        Upgrades.upgradeBeacon(address(beacon), "vault0.3.0/Vault.sol:Vault", opts);
+        assertEq(keccak256(abi.encode(Vault0_2_1(vault).version())), keccak256(abi.encode("v0.3.0")));
+        vm.stopPrank();
+    }
+}
