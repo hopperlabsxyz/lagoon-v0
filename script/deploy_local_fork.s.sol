0655ea309b0fe921cc4e30a46a39132cc0eca4d2
Julien FRANCAIS <jfrancai@student.42.fr>
2025-02-20 11:58:04 +0100

    fix deploy_local_fork

--- script/deploy_local_fork.s.sol
+++ script/deploy_local_fork.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.26;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {InitStruct, Vault} from "@src/v0.2.1/Vault.sol";
+import {IVersion} from "@test/IVersion.sol";
 
 import {DeployBeacon} from "./deploy_beacon.s.sol";
 import {DeployProtocol} from "./deploy_protocol.s.sol";
@@ -40,9 +41,14 @@ contract DeployFull is DeployProtocol, DeployBeacon, DeployVault {
     address VALUATION_MANAGER = vm.envAddress("VALUATION_MANAGER");
 
     function run() external override(DeployProtocol, DeployBeacon, DeployVault) {
+        string memory tag = vm.envString("VERSION_TAG");
         vm.startBroadcast();
         address feeRegistry = deployFeeRegistry(DAO, PROTOCOL_FEE_RECEIVER, PROXY_ADMIN);
-        address beacon = deployBeacon(BEACON_OWNER);
+        address beacon = deployBeacon(BEACON_OWNER, tag);
+        require(
+            keccak256(abi.encode(tag)) == keccak256(abi.encode(IVersion(beacon).version())),
+            "Wrong beacon version deployed"
+        );
         InitStruct memory v = InitStruct({
             underlying: IERC20(UNDERLYING),
             name: NAME,
