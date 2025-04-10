9219522fddb61f29577604240ea257a9709b8c8f
Julien FRANCAIS <jfrancai@student.42.fr>
2025-04-10 20:26:19 +0200

    claimSharesOnBehalf

---
+++ test/v0.4.0/Constants.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import "./VaultHelper.sol";
+
+import {Options, Upgrades} from "@openzeppelin-foundry-upgrades/Upgrades.sol";
+
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {FeeRegistry} from "@src/protocol/FeeRegistry.sol";
+
+import {Test} from "forge-std/Test.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+
+import {BeaconProxyFactory, InitStruct as BeaconProxyInitStruct} from "@src/BeaconProxyFactory.sol";
+
+contract Constants is Test {
+    // ERC20 tokens
+    string network = vm.envString("NETWORK");
+    ERC20Permit immutable USDC = ERC20Permit(vm.envAddress(string.concat("USDC_", network)));
+    ERC20 immutable WETH = ERC20(vm.envAddress(string.concat("WETH_", network)));
+    address immutable WRAPPED_NATIVE_TOKEN = vm.envAddress(string.concat("WRAPPED_NATIVE_TOKEN_", network));
+    ERC20 immutable WBTC = ERC20(vm.envAddress(string.concat("WBTC_", network)));
+    ERC20 immutable ETH = ERC20(vm.envAddress(string.concat("ETH_", network)));
+
+    BeaconProxyFactory factory;
+
+    uint8 decimalsOffset = 0;
+
+    string underlyingName = vm.envString("UNDERLYING_NAME");
+
+    VaultHelper vault;
+    FeeRegistry feeRegistry;
+    string vaultName = "vault_";
+    string vaultSymbol = "hop_vault_";
+    uint256 rateUpdateCooldown = 1 days;
+
+    //Underlying
+    ERC20 underlying = ERC20(vm.envAddress(string.concat(underlyingName, "_", network)));
+    ERC20Permit immutable underlyingPermit;
+
+    // Users
+    VmSafe.Wallet user1 = vm.createWallet("user1");
+    VmSafe.Wallet user2 = vm.createWallet("user2");
+    VmSafe.Wallet user3 = vm.createWallet("user3");
+    VmSafe.Wallet user4 = vm.createWallet("user4");
+    VmSafe.Wallet user5 = vm.createWallet("user5");
+    VmSafe.Wallet user6 = vm.createWallet("user6");
+    VmSafe.Wallet user7 = vm.createWallet("user7");
+    VmSafe.Wallet user8 = vm.createWallet("user8");
+    VmSafe.Wallet user9 = vm.createWallet("user9");
+    VmSafe.Wallet user10 = vm.createWallet("user10");
+    VmSafe.Wallet owner = vm.createWallet("owner");
+    VmSafe.Wallet safe = vm.createWallet("safe");
+    VmSafe.Wallet valuationManager = vm.createWallet("valuationManager");
+    VmSafe.Wallet admin = vm.createWallet("admin");
+    VmSafe.Wallet feeReceiver = vm.createWallet("feeReceiver");
+    VmSafe.Wallet dao = vm.createWallet("dao");
+    VmSafe.Wallet whitelistManager = vm.createWallet("whitelistManager");
+
+    VmSafe.Wallet[] users;
+
+    address[] whitelistInit = new address[](0);
+    bool enableWhitelist = true;
+
+    // Wallet
+    VmSafe.Wallet address0 = VmSafe.Wallet({addr: address(0), publicKeyX: 0, publicKeyY: 0, privateKey: 0});
+
+    int256 immutable bipsDividerSigned = 10_000;
+
+    constructor() {
+        vaultName = string.concat(vaultName, underlyingName);
+        vaultSymbol = string.concat(vaultSymbol, underlyingName);
+
+        vm.label(address(USDC), "USDC");
+        vm.label(address(WETH), "WETH");
+        vm.label(address(ETH), "ETH");
+        vm.label(address(WBTC), "WBTC");
+
+        users.push(user1);
+        users.push(user2);
+        users.push(user3);
+        users.push(user4);
+        users.push(user5);
+        users.push(user6);
+        users.push(user7);
+        users.push(user8);
+        users.push(user9);
+        users.push(user10);
+    }
+
+    function setUpVault(uint16 _protocolRate, uint16 _managementRate, uint16 _performanceRate) internal {
+        feeRegistry = new FeeRegistry(false);
+        feeRegistry.initialize(dao.addr, dao.addr);
+
+        vm.prank(dao.addr);
+        feeRegistry.updateDefaultRate(_protocolRate);
+
+        Options memory opts;
+        opts.constructorData = abi.encode(true);
+        address implementation = Upgrades.deployImplementation("v0.3.0/VaultHelper.sol:VaultHelper", opts);
+        factory = new BeaconProxyFactory(address(feeRegistry), implementation, dao.addr, WRAPPED_NATIVE_TOKEN);
+
+        BeaconProxyInitStruct memory initStruct = BeaconProxyInitStruct({
+            underlying: address(underlying),
+            name: vaultName,
+            symbol: vaultSymbol,
+            safe: safe.addr,
+            whitelistManager: whitelistManager.addr,
+            valuationManager: valuationManager.addr,
+            admin: admin.addr,
+            feeReceiver: feeReceiver.addr,
+            managementRate: _managementRate,
+            performanceRate: _performanceRate,
+            rateUpdateCooldown: rateUpdateCooldown,
+            enableWhitelist: enableWhitelist
+        });
+        address vaultHelper = factory.createVaultProxy(initStruct, keccak256("42"));
+        vault = VaultHelper(vaultHelper);
+
+        if (enableWhitelist) {
+            whitelistInit.push(feeReceiver.addr);
+            whitelistInit.push(dao.addr);
+            whitelistInit.push(safe.addr);
+            whitelistInit.push(vault.pendingSilo());
+            whitelistInit.push(address(feeRegistry));
+            vm.prank(whitelistManager.addr);
+            vault.addToWhitelist(whitelistInit);
+        }
+
+        // console.log(feeReceiver.addr);
+        // console.log(dao.addr);
+        // console.log(assetManager.addr);
+        // console.log(whitelistManager.addr);
+        // console.log(valuationManager.addr);
+        // console.log(admin.addr);
+        // console.log(vault.pendingSilo());
+        // console.log(address(0));
+
+        vm.label(address(vault), vaultName);
+        vm.label(vault.pendingSilo(), "vault.pendingSilo");
+        // vm.label(vault.claimableSilo(), "vault.claimableSilo");
+    }
+}
