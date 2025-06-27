22e420a7a93f65e815ea57aa37d5f00214bb5cf3
Guillaume Dupont <gdupont.pro@gmail.com>
2025-06-27 12:24:02 +0200

    save dependencies

--- script/deploy_protocol.s.sol
+++ script/deploy_protocol.s.sol
@@ -1,50 +1,49 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity 0.8.26;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
 
-// import {TransparentUpgradeableProxy} from
-// "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-// import {FeeRegistry} from "@src/protocol/FeeRegistry.sol";
-// import {Script, console} from "forge-std/Script.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {FeeRegistry} from "@src/protocol/FeeRegistry.sol";
+import {Script, console} from "forge-std/Script.sol";
 
-// import {Options, Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {Options, Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-// /*
-//   run `make protocol` to deploy this script
-// */
+/*
+  run `make protocol` to deploy this script
+*/
 
-// contract DeployProtocol is Script {
-//     function deployFeeRegistry(
-//         address _dao,
-//         address _protocolFeeReceiver,
-//         address _proxyAdmin
-//     ) internal returns (address) {
-//         console.log("--- deployFeeRegistry() ---");
+contract DeployProtocol is Script {
+    function deployFeeRegistry(
+        address _dao,
+        address _protocolFeeReceiver,
+        address _proxyAdmin
+    ) internal returns (address) {
+        console.log("--- deployFeeRegistry() ---");
 
-//         Options memory opts;
-//         opts.constructorData = abi.encode(true);
+        Options memory opts;
+        opts.constructorData = abi.encode(true);
 
-//         TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(
-//             payable(
-//                 Upgrades.deployTransparentProxy(
-//                     "FeeRegistry.sol:FeeRegistry",
-//                     _proxyAdmin,
-//                     abi.encodeWithSelector(FeeRegistry.initialize.selector, _dao, _protocolFeeReceiver),
-//                     opts
-//                 )
-//             )
-//         );
-//         console.log("FeeRegistry proxy address: ", address(proxy));
+        TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(
+            payable(
+                Upgrades.deployTransparentProxy(
+                    "FeeRegistry.sol:FeeRegistry",
+                    _proxyAdmin,
+                    abi.encodeWithSelector(FeeRegistry.initialize.selector, _dao, _protocolFeeReceiver),
+                    opts
+                )
+            )
+        );
+        console.log("FeeRegistry proxy address: ", address(proxy));
 
-//         return address(proxy);
-//     }
+        return address(proxy);
+    }
 
-//     function run() external virtual {
-//         address DAO = vm.envAddress("DAO");
-//         address PROTOCOL_FEE_RECEIVER = vm.envAddress("PROTOCOL_FEE_RECEIVER");
-//         address PROXY_ADMIN = vm.envAddress("PROXY_ADMIN");
+    function run() external virtual {
+        address DAO = vm.envAddress("DAO");
+        address PROTOCOL_FEE_RECEIVER = vm.envAddress("PROTOCOL_FEE_RECEIVER");
+        address PROXY_ADMIN = vm.envAddress("PROXY_ADMIN");
 
-//         vm.startBroadcast();
-//         deployFeeRegistry(DAO, PROTOCOL_FEE_RECEIVER, PROXY_ADMIN);
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.startBroadcast();
+        deployFeeRegistry(DAO, PROTOCOL_FEE_RECEIVER, PROXY_ADMIN);
+        vm.stopBroadcast();
+    }
+}
