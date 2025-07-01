4e84b0787ebcf0966d17b2f0871cd7304e4ab515
Guillaume Dupont <gdupont.pro@gmail.com>
2025-07-01 22:35:59 +0200

    test protocolRegistry

--- test/v0.5.0-lagoonProxy/RatesUpdate.t.sol
+++ test/v0.5.0-lagoonProxy/RatesUpdate.t.sol
@@ -7,7 +7,7 @@ import "forge-std/Test.sol";
 import {BaseTest} from "./Base.sol";
 import {IERC20Metadata, IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {FeeRegistry} from "@src/protocol/FeeRegistry.sol";
+import {ProtocolRegistry} from "@src/protocol-v0.2.0/ProtocolRegistry.sol";
 
 contract testRateUpdates is BaseTest {
     uint16 public constant MAX_MANAGEMENT_RATE = 1000; // 10 %
