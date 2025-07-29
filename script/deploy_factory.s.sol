e8d1171ef4697d0e4919d16a885db16eea73aaeb
Guillaume Dupont <gdupont.pro@gmail.com>
2025-07-29 09:23:52 +0200

    testing build package process

--- script/deploy_factory.s.sol
+++ script/deploy_factory.s.sol
@@ -14,24 +14,6 @@ import {IVersion} from "@test/IVersion.sol";
 */
 
 contract DeployBeaconProxyFactory is Script {
-    // function deployBeaconProxyFactory(
-    //     address _registry,
-    //     address _implementation,
-    //     address _owner,
-    //     address _wrappedNativeToken
-    // ) internal returns (address) {
-    //     console.log("--- deployBeaconProxyFactory() ---");
-    //     console.log("Protocol registry:  ", _registry);
-    //     console.log("Implementation:  ", _implementation);
-    //     console.log("Owner:  ", _owner);
-
-    //     BeaconProxyFactory beaconProxyFactory =
-    //         new BeaconProxyFactory(_registry, _implementation, _owner, _wrappedNativeToken);
-
-    //     console.log("Beacon proxy factory  address:", address(beaconProxyFactory));
-    //     return address(beaconProxyFactory);
-    // }
-
     function run() external virtual {
         address FEE_REGISTRY = vm.envAddress("REGISTRY");
         address DAO = vm.envAddress("DAO");
